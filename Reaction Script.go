2a3a4a50ae0679f48a574240bc01addd0e765273
Longhouse81 <bartmeeus@hotmail.com>
2021-09-15 17:06:12 +0200

    Update converter

--- Reaction Script.go
+++ Reaction Script.go
@@ -2,7 +2,7 @@
 
 {{/*Converter for interfase types from DB*/}}
 {{- define "standardize"}}
-    {{- $types:=sdict `templates.SDict` 1 `templates.Slice` 1 `map[string]interface {}` 2 `[]interface {}` 3}}
+	{{- $types:=sdict `templates.SDict` 1 `templates.Slice` 1 `map[string]interface {}` 2 `[]interface {}` 3 `*templates.Slice` 3 `*templates.SDict` 2}}
     {{- $rVal:= ""}}
     {{- if $t:= $types.Get (printf "%T" .val)}}
         {{- if eq $t 1}}{{$rVal = .val}}{{else if eq $t 2}}{{$rVal = sdict .val}}{{else if eq $t 3}}{{$rVal = cslice.AppendSlice .val}}{{end}}
@@ -14,19 +14,9 @@
 {{if .ReactionAdded}}
 	{{$delReaction:= 1}}
 	{{$reaction:=(print (or (and ($x:=($z:=.Reaction.Emoji).Animated) "a:") "") $z.Name (or (and $z.ID (print ":" $z.ID)) ""))}}
-	{{if ($inst:= (dbGet 0 (print "EM" ($cID:= .Reaction.ChannelID) ($mID:= .Reaction.MessageID))).Value)}}
-		{{/*template "standardize" ($x:= sdict "val" $db)}}{{$inst:= $x.ret}}
-		{{$db}}
-		{{(printf "%T" $db)}}
-		{{(printf "%T" (index $db.defemoji 0))}}
-		{{(printf "%T" $db.duration)}}
-		{{(printf "%T" (index $db.embeds 0))}}
-		{{(printf "%T" $db.mentionarg)}}
-		{{(printf "%T" $db.publicargarg)}}
-		{{(printf "%T" (index $db.users 0))*/}}
-		```{{(printf "%T" .Reaction.UserID)}}```
-		```{{(printf "%T" (index $inst.users 0))}}```
-		{{if or (not $inst.users) (in $inst.users .Reaction.UserID)}}in users
+	{{if ($db:= (dbGet 0 (print "EM" ($cID:= .Reaction.ChannelID) ($mID:= .Reaction.MessageID))).Value)}}
+		{{template "standardize" ($x:= sdict "val" $db)}}{{$inst:= $x.ret}}
+		{{if or (not $inst.users) (in $inst.users .Reaction.UserID)}}
 			{{$match:= 0}}
 			{{$pos:= 0}}
 			{{range $i, $e:= $inst.embeds}}{{if .emoji}}{{if eq .emoji $reaction}}{{$pos = $i}}{{$match = 1}}{{end}}{{end}}{{end}}
